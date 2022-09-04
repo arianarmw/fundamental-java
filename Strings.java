15899083a1a6d8e219592c65911e73a216ea4d6b
Ariana Rahmawati <98734080+arianarmw@users.noreply.github.com>
2022-09-04 21:32:20 +0700

    mengerjakan soal 5 strings

---
+++ Strings.java
@@ -0,0 +1,17 @@
+//package pertemuan2;
+//
+//import java.util.Scanner;
+//
+//public class Strings {
+//	public static void main(String[] args) {
+//		Scanner myObj = new Scanner(System.in);
+//		String A = myObj.nextLine();
+//		String B = myObj.nextLine();
+//		A = A.substring(0,1).toUpperCase() + A.substring(1);
+//		B = B.substring(0,1).toUpperCase() + B.substring(1);
+//		String compare = (A.substring(0,1).compareTo(B.substring(0,1)) > 0)?"yes":"no";
+//		System.out.println(A.length() + B.length());
+//		System.out.println(compare);
+//		System.out.println(A + " " + B);
+//	}
+//}
